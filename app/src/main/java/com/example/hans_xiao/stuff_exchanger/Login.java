7b053e21fee930d56ad5d10dbf8263a05976cf15
YUNHAN XIAO <2028427786@qq.com>
2018-07-18 14:51:59 +0900

    Version 7:incompleted:can work with firebase now

--- app/src/main/java/com/example/hans_xiao/stuff_exchanger/Login.java
+++ app/src/main/java/com/example/hans_xiao/stuff_exchanger/Login.java
@@ -1,8 +1,11 @@
 package com.example.hans_xiao.stuff_exchanger;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -14,5 +17,12 @@ public class Login extends AppCompatActivity {
         if(actionbar != null) {
             actionbar.hide();
         }
+        Button button_signup = (Button) findViewById(R.id.signup_button);
+        button_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Intent intent = new Intent();
+            }
+        });
     }
 }
